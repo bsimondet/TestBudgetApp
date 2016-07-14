337fac70d1900dba413796cabde8e5be7823b3fc
bsimondet <ben.simondet@gmail.com>
2016-07-14 11:32:22 -0500

    first commit

---
+++ TestBudget/Controllers/AddTransactionController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestBudget.Models;
+
+namespace TestBudget.Controllers
+{
+    public class AddTransactionController : Controller
+    {
+        // GET: AddTransaction
+        public ActionResult Index(AddTransactionViewModel model)
+        {
+            return View(model);
+        }
+    }
+}
